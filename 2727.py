54b7495c68fb438bce5577209b1685e2b6cd41b1
Bernardo Castello <73943159+BernardoCastello@users.noreply.github.com>
2022-05-09 10:00:29 -0300

    Create 2727.py

---
+++ 2727.py
@@ -0,0 +1,60 @@
+while True:
+    try:
+        n = int(input())
+        for i in range(n):
+                    lin = input()
+
+                    if (lin == "."):
+                        print("a")
+                    elif (lin == ".."):
+                        print("b")
+                    elif (lin == "..."):
+                        print("c")
+                    elif (lin == ". ."):
+                        print("d")
+                    elif (lin == ".. .."):
+                        print("e")
+                    elif (lin == "... ..."):
+                        print("f")
+                    elif (lin == ". . ."):
+                        print("g")
+                    elif (lin == ".. .. .."):
+                        print("h")
+                    elif (lin == "... ... ..."):
+                        print("i")
+                    elif (lin == ". . . ."):
+                        print("j")
+                    elif (lin == ".. .. .. .."):
+                        print("k")
+                    elif (lin == "... ... ... ..."):
+                        print("l")
+                    elif (lin == ". . . . ."):
+                        print("m")
+                    elif (lin == ".. .. .. .. .."):
+                        print("n")
+                    elif (lin == "... ... ... ... ..."):
+                        print("o")
+                    elif (lin == ". . . . . ."):
+                        print("p")
+                    elif (lin == ".. .. .. .. .. .."):
+                        print("q")
+                    elif (lin == "... ... ... ... ... ..."):
+                        print("r")
+                    elif (lin == ". . . . . . ."):
+                        print("s")
+                    elif (lin == ".. .. .. .. .. .. .."):
+                        print("t")
+                    elif (lin == "... ... ... ... ... ... ..."):
+                        print("u")
+                    elif (lin == ". . . . . . . ."):
+                        print("v")
+                    elif (lin == ".. .. .. .. .. .. .. .."):
+                        print("w")
+                    elif (lin == "... ... ... ... ... ... ... ..."):
+                        print("x")
+                    elif (lin == ". . . . . . . . ."):
+                        print("y")
+                    elif (lin == ".. .. .. .. .. .. .. .. .."):
+                        print("z")
+    except EOFError:
+        exit()
