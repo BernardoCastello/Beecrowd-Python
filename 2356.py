28345067b38d4f2e85c5a412a3e55ed53787d1eb
Bernardo Castello <73943159+BernardoCastello@users.noreply.github.com>
2022-05-09 11:11:49 -0300

    Create 2356.py

---
+++ 2356.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        a = input()
+        b = input()
+
+        if b in a:
+            print("Resistente")
+        else:
+            print("Nao resistente")
+
+    except EOFError:
+        break
